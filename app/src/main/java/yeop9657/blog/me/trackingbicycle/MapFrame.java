4e6dc317d6fa6415411de97b3e8a75386d5a8caf
양창엽 <ChangYeop-Yang@users.noreply.github.com>
2017-12-10 21:52:49 +0900

    [Mobile App Programming 1] Team School Android Project. (Prof. 정기숙) Update GoogleMap 2017.12.10.

--- app/src/main/java/yeop9657/blog/me/trackingbicycle/MapFrame.java
+++ app/src/main/java/yeop9657/blog/me/trackingbicycle/MapFrame.java
@@ -7,17 +7,36 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 /**
  * Created by 양창엽 on 2017-12-09.
  */
 
-public class MapFrame extends Fragment {
+public class MapFrame extends Fragment implements OnMapReadyCallback{
+
+    /* MARK - : GoogleMap */
+    private GoogleMap mGoogleMap = null;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View mView = inflater.inflate(R.layout.frame_map, container, false);
 
+        final SupportMapFragment mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mv_MapView);
+        mMapFragment.getMapAsync(this);
+
         return mView;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        googleMap.getUiSettings().setCompassEnabled(true);
     }
 }
